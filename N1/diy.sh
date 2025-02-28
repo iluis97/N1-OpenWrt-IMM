78599a26265481fe0b523beb863620cd5dce4a35
Lius <51404060+iluis97@users.noreply.github.com>
2025-02-28 21:20:05 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+# 文件中每一个出现 192.168.1.1 的地方都会被替换为 192.168.5.5
+sed -i 's/192.168.1.1/192.168.5.5/g' package/base-files/files/bin/config_generate
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
